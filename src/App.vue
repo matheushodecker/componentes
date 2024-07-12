ab8136cb027d8635c3d6494f1fb172630567150d
matheus.hodekcer <matheus.hodecker.santos@gmail.com>
2024-07-12 15:06:27 -0300

    aula components

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue';
+</script>
+
+<template>
+    <h1>componentes</h1>
+    <hr>
+    <ExpandBox title="caixa1" content="ain nobru apelão"/>
+    <expand-box title="caixa2" content="ain nobru apelão 2"/>
+</template>
+
+<style scoped></style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, computed } from 'vue';
+const props = defineProps(['title', 'content']);
+const showBox = ref(false)
+const textoBotao = computed(() =>
+    showBox.value ? 'esconder' : 'Mostrar'
+)
+</script>
+
+<template>
+    <button @click="showBox = !showBox">{{textoBotao}}</button>
+
+<div v-if="showBox" class="expandbox">
+    <h1>{{ props.title }}</h1>
+    <p>{{ props.content }}</p>
+</div>
+
+</template>
+
+<style scoped>
+.expandbox {
+    height: 100%;
+    padding: 20px;
+    border-radius: 10px;
+    border: 1px solid black;
+    box-shadow: 3px 3px 2px 2px gray;
+}
+</style>
